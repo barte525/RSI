b37d050543309940a08979fa101d471d129943a8
ale-gen <o.generowicz@gmail.com>
2022-03-15 18:54:46 +0100

    Add Dockerfile & docker-compose

---
+++ howMoneyServer/data/ApiDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace howMoneyServer.data
+{
+    public class ApiDbContext: DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext>options)
+        {
+        }
+
+    }
+}
