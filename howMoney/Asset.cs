216b196bb4ac5914b3c206baa0a28ecbd87b8cf2
ale-gen <o.generowicz@gmail.com>
2022-03-17 23:29:36 +0100

    Add .NET project with db connection

---
+++ howMoney/Asset.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace howMoney
+{
+    public class Asset
+    {
+       [Key]
+       public int Id { get; set; }
+       public string Type { get; set; }
+    }
+}
