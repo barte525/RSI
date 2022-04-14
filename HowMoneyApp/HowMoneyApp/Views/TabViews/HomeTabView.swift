d622ed4257fe58ab0d60d2be4bfcc6aca3dc59c0
ale-gen <o.generowicz@gmail.com>
2022-04-15 00:53:42 +0200

    Create home view

--- HowMoneyApp/HowMoneyApp/Views/TabViews/HomeTabView.swift
+++ HowMoneyApp/HowMoneyApp/Views/TabViews/HomeTabView.swift
@@ -8,9 +8,29 @@
 import SwiftUI
 
 struct HomeTabView: View {
+    
+    @State var currencyPreferenceChoice: String = K.preferenceCurrencies[0]
+    @State var totalBudget: Double = 0.0
+    
     var body: some View {
         VStack {
-            Text("Home")
+            VStack {
+                Text("Total Budget")
+                    .foregroundColor(Color.white)
+                    .font(.system(size: 20))
+                    .padding(.bottom, 15)
+                Text("\(currencyPreferenceChoice) \(totalBudget)")
+                    .font(.largeTitle)
+                    .foregroundColor(Color.white)
+            }
+            .frame(maxWidth: .infinity)
+            .frame(height: 150)
+            .background(Color("Lavenda"))
+            .cornerRadius(10)
+            .padding([.leading, .trailing], 30)
+            .padding(.top, 20)
+            
+            Spacer()
         }
         .background(Color("Background"))
     }
