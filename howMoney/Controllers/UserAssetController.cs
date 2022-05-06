0079682144f2d7fe829d98fc8edba0885df4cd48
ale-gen <o.generowicz@gmail.com>
2022-05-06 23:31:56 +0200

    Add request to get assets' sum of user

--- howMoney/Controllers/UserAssetController.cs
+++ howMoney/Controllers/UserAssetController.cs
@@ -19,12 +19,14 @@ namespace howMoney.Controllers
         private readonly ILogger<UserAssetController> _logger;
         private readonly IRepository<UserAsset> _userAssetRepository;
         private readonly IRepository<User> _userRepository;
+        private readonly IRepository<Asset> _assetRepository;
 
-        public UserAssetController(ILogger<UserAssetController> logger, IRepository<UserAsset> userAssetRepository, IRepository<User> userRepository)
+        public UserAssetController(ILogger<UserAssetController> logger, IRepository<UserAsset> userAssetRepository, IRepository<User> userRepository, IRepository<Asset> assetRepository)
         {
             _logger = logger;
             _userAssetRepository = userAssetRepository;
-            _userRepository = userRepository;  
+            _userRepository = userRepository;
+            _assetRepository = assetRepository;
         }
 
         [HttpGet("userId"), Authorize]
@@ -45,6 +47,37 @@ namespace howMoney.Controllers
             return null;
         }
 
+        [HttpGet("sum/{currencyPref}/{userId}"), Authorize]
+        public double? GetSum(String currencyPref, Guid userId)
+        {
+            if (Authenticate_user(User.FindFirstValue(ClaimTypes.Email), userId))
+            {
+                double sum = 0;
+                double converter;
+                string chosenCurrencyName = "Converter" + currencyPref;
+
+                IEnumerable<UserAsset> userAssets = _userAssetRepository.GetAll(userId);
+
+                foreach (var userAsset in userAssets)
+                {
+                    try
+                    {
+                        Asset asset = _assetRepository.GetById(userAsset.AssetId);
+                        var value = asset.GetType().GetProperty(chosenCurrencyName).GetValue(asset, null);
+                        converter = (double)value;
+                        sum += userAsset.Amount * converter;
+                    } catch {
+                        HttpContext.Response.StatusCode = 401;
+                        return null;
+                    }
+                }
+                return sum;
+            }
+
+            HttpContext.Response.StatusCode = 401;
+            return null;
+        }
+
         [HttpPost, Authorize]
         public async Task<Object> Post([FromBody] UserAsset userAsset)
         {
@@ -57,7 +90,6 @@ namespace howMoney.Controllers
                 }
                 catch (Exception ex)
                 {
-
                     return ex;
                 }
             }
