312b340bd957686a209f8e4732ab81c48dc7cc93
barte525 <bartek5251@gmail.com>
2022-06-15 13:26:42 +0200

    new DTO

---
+++ howMoney/Models/AlertWithIdDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace howMoney.Models
+{
+    public class AlertWithIdDTO
+    {
+        public int Id { get; set; } = 0;
+        public string asset_name { get; set; } = string.Empty;
+        public string currency { get; set; } = string.Empty;
+        public double value { get; set; } = 0.0;
+
+    }
+}
